2e3eda744d9e41c190be7e47bcfa6e7d804eb9f0
Philip Howley <howley.phil@gmail.com>
2022-07-23 13:10:38 +0100

    react prop destructuring

---
+++ review-week-02/src/components/CampsiteCard.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// const CampsiteCard = (props) => {
+const CampsiteCard = ({ campsite }) => {
+  // console.log(props)
+  console.log(campsite)
+  return <div>CampsiteCard</div>
+}
+
+export default CampsiteCard
