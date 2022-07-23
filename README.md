efcc562e1c670c66a9d01acd567c38919aa6e5b2
Philip Howley <howley.phil@gmail.com>
2022-07-23 13:04:42 +0100

    destructuring

---
+++ README.md
@@ -0,0 +1,4 @@
+# Destructuring in parameter list
+
+- In React
+- All components have prop object
---
+++ index.js
@@ -0,0 +1,5 @@
+// <Campsite campsite={campsite} />
+
+// const Campsite = (props) => {
+//   const campsite = props.campsite
+// }
